28ab3e7e8c973ebf5ce45605dbc053c11181a625
dali918 <dalitbanda@gmail.com>
2024-05-08 19:54:16 -0400

    start homework3 with gazebo_project tutorial code

--- homework3/setup.py
+++ homework3/setup.py
@@ -1,3 +1,5 @@
+import os 
+from  glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'homework3'
@@ -10,12 +12,17 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='dalitso',
     maintainer_email='dalitbanda@gmail.com',
-    description='TODO: Package description',
+    description='homework3 package to build a robot graphic',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
